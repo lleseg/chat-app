79e35b7411dde16eb5b7eed9c42f02d5bdf87ac7
Franco De Nardo <fdenardo.utn@gmail.com>
2025-01-17 17:15:47 -0300

    feat: :sparkles: add rooms scaffold

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,45 @@
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room, only: %i[show edit update destroy]
+
+  # GET /rooms or /rooms.json
+  def index
+    @rooms = Room.all
+  end
+
+  # GET /rooms/1 or /rooms/1.json
+  def show; end
+
+  # GET /rooms/new
+  def new
+    @room = Room.new
+  end
+
+  # GET /rooms/1/edit
+  def edit; end
+
+  # POST /rooms or /rooms.json
+  def create
+    @room = Room.new(room_params)
+  end
+
+  # PATCH/PUT /rooms/1 or /rooms/1.json
+  def update; end
+
+  # DELETE /rooms/1 or /rooms/1.json
+  def destroy
+    @room.destroy!
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
